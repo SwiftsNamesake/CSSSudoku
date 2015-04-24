5c56481bf71696eb72ffb394d6fc102bff687185
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-04-24 23:00:37 +0200

    1)

--- sudoku.js
+++ sudoku.js
@@ -6,4 +6,18 @@
 
 $(document).ready(function() {
 
+	/**/
+	console.log('Document ready')
+
+	$('body').keypress(function(ev) {
+		console.log('Processing key press on board');
+		var selected = $('.subgrid tr td:hover');
+		var key      = String.fromCharCode(ev.which);
+		if ('123456789'.indexOf(key) > (-1)) {
+			/* A numeric key (not zero) was pressed */
+			selected.html(key);
+		}
+	});
+
+
 });
