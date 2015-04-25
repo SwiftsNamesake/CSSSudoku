d773efe98e26c7d211ebd73a9bbb3f65ab6439cd
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-04-25 21:12:26 +0200

    1) Added introduction files (.html, .js, .css) 2) Added TODO.md 3) Moved intro css to a separate stylesheet 4) Did some work on the Sudoku logic object 5) Added temporary favicon

---
+++ TODO.md
@@ -0,0 +1,6 @@
+#
+
+1. Introduction (animated, sounds)
+2. Cookies (users, highscores, times)
+3. Sound effects (?)
+4. Tutorial (with overlays, opt-out, remember returning users)
--- sudoku.js
+++ sudoku.js
@@ -4,6 +4,59 @@
  */
 
 
+
+function Sudoku() {
+	
+	/**/
+
+	// TODO: Decide on API (constructor function or namespace object)
+
+	if (this === window) {
+		console.error('Constructor \'Sudoku\' used as a normal function (without new).');
+		return;
+	}
+
+
+	this.board = [[0,0,0,0,0,0,0,0,0],
+	              [0,0,0,0,0,0,0,0,0],
+	              [0,0,0,0,0,0,0,0,0],
+	              [0,0,0,0,0,0,0,0,0],
+	              [0,0,0,0,0,0,0,0,0],
+	              [0,0,0,0,0,0,0,0,0],
+	              [0,0,0,0,0,0,0,0,0],
+	              [0,0,0,0,0,0,0,0,0],
+	              [0,0,0,0,0,0,0,0,0]];
+
+}
+
+
+Sudoku.prototype.validColumn = function(icol) {
+	filled = this.board[icol].filter(function(n) { return n != 0; }); // A list of all filled (non-zero) tiles in the column
+
+}
+
+
+Sudoku.prototype.validRow = function(irow) {
+	return [0,1,2,3,4,5,6,7,8].every(function(icol) {
+
+	});
+}
+
+
+Sudoku.prototype.validSubgrid = function(icol, irow) {
+	var subgrid = [0,1,2,3,4,5,6,7,8].map(function(n) { return this.board[3*icol + (n%3)][3*irow + Math.floor(irow/3)]; });
+	return [0,1,2,3,4,5,6,7,8].every(function(n) { return subgrid.indexOf(n) ; });
+}
+
+
+
+function validBoard(board) {
+	return [1,2,3,4,5,6,7,8].every(function(n) {
+		return sudoku.validRow(n) && sudoku.validColumn(n) && sudoku.validSubgrid(n%3, Math.floor(n/3));
+	});
+}
+
+
 $(document).ready(function() {
 
 	/**/
@@ -16,6 +69,7 @@ $(document).ready(function() {
 		if ('123456789'.indexOf(key) > (-1)) {
 			/* A numeric key (not zero) was pressed */
 			selected.html(key);
+			// selected.
 		}
 	});
 
